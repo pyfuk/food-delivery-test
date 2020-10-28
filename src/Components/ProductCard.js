09c284648255f3798d21672978bb2ffc31e6a35d
Shakhboz Khalikov <cool_man1111@mail.ru>
2020-10-28 20:48:07 +0500

    [+] Added product counter

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,8 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
 function ProductCard(props) {
+  const [button, setPressed] = useState({
+    pressed: false,
+    count: 0,
+  });
+
+  let addButoon = null;
+
+  if (button.pressed) {
+    addButoon = (
+      <div className="product-card-buttons">
+        <button
+          className="product-card-button-minus"
+          onClick={() => minusCount()}
+        >
+          <FontAwesomeIcon icon={faMinus} />
+        </button>
+        <div className="product-card-button-counter">{button.count} шт</div>
+        <button
+          className="product-card-button-plus"
+          onClick={() => plusCount()}
+        >
+          <FontAwesomeIcon icon={faPlus} />
+        </button>
+      </div>
+    );
+  }
+
+  if (!button.pressed) {
+    addButoon = (
+      <button
+        className="product-card-button"
+        onClick={() => plusCount(props.product)}
+      >
+        <FontAwesomeIcon icon={faPlus} />
+      </button>
+    );
+  }
+
+  const plusCount = (product) => {
+    setPressed({ pressed: true, count: button.count + 1 });
+  };
+  const minusCount = () => {
+    if (button.count === 1) {
+      setPressed({ pressed: false, count: button.count - 1 });
+    } else {
+      setPressed({ pressed: true, count: button.count - 1 });
+    }
+  };
+
   return (
     <div className="product-card-container">
       <div className="product-card-shop-wrap">{props.product.shopCode}</div>
@@ -19,9 +69,7 @@ function ProductCard(props) {
             {props.product.price}
             <span className="product-card-price-ruble">&#8381;</span>
           </p>
-          <button className="product-card-button">
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
+          {addButoon}
         </div>
       </div>
     </div>
