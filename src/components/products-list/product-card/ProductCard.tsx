ae3b27073a297c8bedc81c476b3d7c684368e70e
Shakhboz Khalikov <cool_man1111@mail.ru>
2020-11-09 12:19:56 +0500

    [+]Added selected product style

--- src/components/products-list/product-card/ProductCard.tsx
+++ src/components/products-list/product-card/ProductCard.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Product } from "../../../types/ProductModel";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
 
 import s from "./ProductCard.module.scss";
 
@@ -11,8 +12,11 @@ interface ProductParam {
 }
 
 const ProductCard = ({ product }: ProductParam) => {
+  const [active, setActive] = useState(false);
   return (
-    <div className={s.product_container}>
+    <div
+      className={active ? s.product_container_selected : s.product_container}
+    >
       <div className={s.shop}>
         <span>Smth</span>
       </div>
@@ -20,15 +24,29 @@ const ProductCard = ({ product }: ProductParam) => {
         <Link to={"/"}>
           <div className={s.image_container}>
             <img src={product.images[0].path} alt="product" />
+            <div className={s.selected}>
+              <div className={s.circle}>
+                <img
+                  src={process.env.PUBLIC_URL + "/product/basket.svg"}
+                  alt="cart"
+                ></img>
+              </div>
+            </div>
           </div>
           <p className={s.name}>{product.name}</p>
           <p className={s.weight}>{product.weight} г</p>
         </Link>
-        <div className={s.closePrice}>
+        <div className={s.price}>
           <p>{product.price}</p>
-          <button>
-            <FontAwesomeIcon icon={faPlus} />
-          </button>
+          <div className={s.buttons}>
+            <button className={s.minusButton} onClick={() => setActive(false)}>
+              <FontAwesomeIcon icon={faMinus} />
+            </button>
+            <div className={s.counterValue}>0 шт</div>
+            <button className={s.plusButton} onClick={() => setActive(true)}>
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
